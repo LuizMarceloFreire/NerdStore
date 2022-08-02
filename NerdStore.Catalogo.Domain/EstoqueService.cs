127e0d9e80e2825b82b028d1deb3645210a4b2ba
Luiz Marcelo Freire da S. Fialho <cast.luiz.utic@mt.sebrae.com.br>
2022-08-02 12:05:37 -0300

    Implementação de um serviço de negocio.

---
+++ NerdStore.Catalogo.Domain/EstoqueService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class EstoqueService : IEstoqueService
+    {
+        private readonly IProdutoRepositorio _produtoRepositorio;
+
+        public EstoqueService(IProdutoRepositorio produtoRepositorio)
+        {
+            _produtoRepositorio = produtoRepositorio;   
+        }
+
+        public async Task<bool> DebitarEstoque(Guid produtoId, int quantidade)
+        {
+            var produto = await _produtoRepositorio.ObterPorId(produtoId);
+
+            if (produto == null) return false;
+
+            if(!produto.PossuiEstoque(quantidade)) return false;
+
+            produto.DebitarEstoque(quantidade);
+
+            _produtoRepositorio.Atualizar(produto);
+            return await _produtoRepositorio.UnitOfWork.Commit();
+        }
+
+        public async Task<bool> ReporEstoque(Guid produtoId, int quantidade)
+        {
+            var produto = await _produtoRepositorio.ObterPorId(produtoId);
+
+            if (produto == null) return false;
+
+            produto.ReporEstoque(quantidade);
+
+            _produtoRepositorio.Atualizar(produto);
+            return await _produtoRepositorio.UnitOfWork.Commit();
+        }
+
+        public void Dispose()
+        {
+            _produtoRepositorio?.Dispose();
+        }
+    }
+}
